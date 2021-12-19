c2e67e964ac748b57aa804c4a11d2c22680f0644
Alyssa Falcione <lyzarddz@gmail.com>
2021-12-19 01:09:18 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+
+function superbowlWin(arr){
+   const winner = arr.find(el => el.result === "W");
+   if (winner){
+       return winner.year
+   } else {
+       return undefined;
+   }
+}
+
